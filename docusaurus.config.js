f73df5b9b5454c133eb1dee8f4b0c46b6c81822e
Swaroop Hegde <email@swaroophegde.com>
2022-12-07 20:32:27 +0530

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,169 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "PowerLoom Pooler Docs",
+  tagline: "Documentation for PowerLoom Pooler",
+  url: "https://pooler-docs.powerloom.io/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "powerloom", // Usually your GitHub org/user name.
+  projectName: "pooler-docs", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          docLayoutComponent: "@theme/DocPage",
+          docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
+        },
+        /*
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+        },
+        */
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs: {
+        sidebar: {
+          hideable: true
+        }
+      },
+      navbar: {
+        title: "Pooler Docs",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg"
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Intro"
+          },
+          {
+            label: "Petstore API",
+            position: "left",
+            to: "/docs/category/petstore-api"
+          },
+          {
+            label: "Pooler API",
+            position: "left",
+            to: "/docs/category/pooler-api"
+          },
+          {
+            href: "https://github.com/PowerLoom/pooler-docs",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Intro",
+                to: "/docs/intro"
+              }
+            ]
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.gg/WkYeZK3hC8"
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/PowerLoomHQ"
+              }
+            ]
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/powerloom"
+              }
+            ]
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} PowerLoom, Inc. Built with Docusaurus.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["ruby", "csharp", "php"]
+      }
+    }),
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          petstore: {
+            specPath: "examples/petstore.yaml",
+            outputDir: "docs/petstore",
+            downloadUrl:
+              "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag"
+            }
+          },
+          pooler: {
+            specPath: "examples/pooler.yaml",
+            outputDir: "docs/pooler",
+            downloadUrl:
+              "https://files.slack.com/files-pri/T01BM7EKF97-F04E0GYA8UD/out.yml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag"
+            }
+          }
+        }
+      }
+    ]
+  ],
+
+  themes: ["docusaurus-theme-openapi-docs"]
+};
+
+module.exports = config;
